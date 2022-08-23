f0076d75ae0f59e072512f9adbb681fcaf76a416
mabarbar <mateusz.barzenc00@gmail.com>
2022-08-24 01:04:48 +0200

    made progress with api

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-const message = document.querySelector(".rate-info");
+let message = document.querySelector(".rate-info");
 const button = document.querySelector("button");
 const box_one = document.querySelector(".first-box");
 const box_two = document.querySelector(".second-box");
 const selection_one = document.querySelector("#currency-one");
 const selection_two = document.querySelector("#currency-two");
 
-const API_KEY = "OqqnI2T4mgCMOVcckWcv83z84vvtCx8C"
-const API_LINK = "https://api.apilayer.com/exchangerates_data/convert?to=" + selection_one.value + "&from=" + selection_two.value + "&amount=" + box_one.value
-
+const API_KEY = "OqqnI2T4mgCMOVcckWcv83z84vvtCx8C";
+const API_LINK = `https://api.apilayer.com/exchangerates_data/convert?to=
+  ${selection_two.value}&from=${selection_one.value}&amount=${box_one.value}`;
 
 const myHeaders = new Headers();
 myHeaders.append("apikey", API_KEY);
 
 const requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
+  method: "GET",
+  redirect: "follow",
+  headers: myHeaders,
 };
 
-// fetch(API_LINK, requestOptions)
-//   .then(response => response.json())
-//   .then(res => console.log(res))
-//   .catch(error => console.log('error', error));
+const convert = () => {
+  // fetch(API_LINK, requestOptions)
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     const rate = res.info[1];
+  //     message.textContent = `Exchange rate is ${rate}`;
+  //     box_two.value = res.result;
+  //     console.log(res);
+  //     console.log(res.info);
+  //     console.log(rate);
+  // })
+  // .catch((error) => console.log("error", error));
+  console.log(API_LINK);
+  console.log(box_one.value);
+};
 
-button.addEventListener("click", convert)
+button.addEventListener("click", convert);
